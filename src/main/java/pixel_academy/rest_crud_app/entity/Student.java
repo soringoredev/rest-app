057ccc3cae40e72a135a0fc750ecb8d0abe0f191
Sorin Gore <soringoredev@gmail.com>
2025-08-15 19:57:21 +0300

    Add new rest

---
+++ src/main/java/pixel_academy/rest_crud_app/entity/Student.java
@@ -0,0 +1,12 @@
+package pixel_academy.rest_crud_app.entity;
+
+
+public class Student {
+
+    private String name;
+
+
+//    public String getName() {
+//        return ;
+//    }
+}
---
+++ src/main/java/pixel_academy/rest_crud_app/rest/StudentRestController.java
@@ -0,0 +1,4 @@
+package pixel_academy.rest_crud_app.rest;
+
+public class StudentRestController {
+}
