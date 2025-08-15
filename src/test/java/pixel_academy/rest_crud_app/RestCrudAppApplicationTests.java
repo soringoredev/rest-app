9a2d0fffac99e76ca4e4e10145e301e9827ae4f9
Sorin Gore <soringoredev@gmail.com>
2025-08-15 18:49:16 +0300

    Initial commit

---
+++ src/test/java/pixel_academy/rest_crud_app/RestCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel_academy.rest_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
